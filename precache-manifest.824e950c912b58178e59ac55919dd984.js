957d7165f0b614a35b5e2e10d6b50d065b5916ad
LloydTao <41923457+LloydTao@users.noreply.github.com>
2020-08-12 19:34:13 +0000

    Deploying to build from  @ 3c6ca189f665a0d108860c7f1b3c7d09f169525e 🚀

---
+++ precache-manifest.824e950c912b58178e59ac55919dd984.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82c6251d9b6cb6f9002df299c0960dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f00d8fa94430cfb3c75",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44efbeb1a345ba240663",
+    "url": "/static/js/2.6b633340.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b633340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f00d8fa94430cfb3c75",
+    "url": "/static/js/main.6b652db0.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
